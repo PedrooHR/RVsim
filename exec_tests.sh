907114c410d95652bba36a31726af135421556bf
Pedro Henrique Rosso <pedro.rosso@ufabc.edu.br>
2020-11-20 16:47:23 -0300

    add benchmarks including adpcm (rawcaudio.bin), update exec all script

--- exec_tests.sh
+++ exec_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for f in testes/bin/*.bin; do
+export PRINT_GSHARE_LOG=true
+export PRINT_CACHE_MISSES_LOG=true
+
+for f in benchmarks/*.bin; do
 	echo "Executing $f"
 	./rvsim $f 4 10
 	echo 
